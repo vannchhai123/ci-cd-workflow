716c8872b931224d2bac3517c8fd1179daac0421
Vannchhai <vannchhai.sti@gmail.com>
2026-02-14 10:39:40 +0700

    first commit

---
+++ src/main/java/com/example/ci_cd_workflow/CiCdWorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.example.ci_cd_workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdWorkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdWorkflowApplication.class, args);
+	}
+
+}
